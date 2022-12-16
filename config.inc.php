2ab6dc2b1bdef6053629caa47b83903f4511e184
JF <j.fercher@web.de>
2022-12-16 16:01:10 +0100

    Add files via upload

---
+++ config.inc.php
@@ -0,0 +1,13 @@
+<?php
+	//Append a copy of this section for each site
+	//Site ID: choose unique consecutive ID starting from 1 (eg. 1, 2, 3, etc) 
+	$sid=1;
+	//The site label
+	$sitename[$sid]="your_site.xxx";
+	$dbname="your.db";
+	$dbname1="your1.db";
+	//Table name
+	$tablename[$sid]="yourtable";
+	$tablename1[$sid]="yourtable1";
+?>
+
