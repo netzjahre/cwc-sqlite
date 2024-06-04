bb7e6afb1be900d73835686e3e2458942027d1e0
JF <j.fercher@web.de>
2024-06-04 14:21:03 +0200

    Update config.inc.php
    2024-06-04

--- config.inc.php
+++ config.inc.php
@@ -3,12 +3,12 @@
 	//Site ID: choose unique consecutive ID starting from 1 (eg. 1, 2, 3, etc) 
 	$sid=1;
 	//The site label
-	$sitename[$sid]="netzjahre.com";
+	$sitename[$sid]="my_site.tld";
 	$dbname="cwcsqlite.db";
 	$dbname1="cwcount.db";
 	//Table name
-	$tablename[$sid]="cwcsqlite";
-	$tablename1[$sid]="cwctimport";
+	$tablename[$sid]="cwsqlite";
+	$tablename1[$sid]="cwtimport";
 	date_default_timezone_set('Europe/Berlin');
 ?>
 
